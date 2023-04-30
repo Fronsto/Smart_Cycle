3986825529842c6046fce75513c754fbcdf8fb25
Gunjan Dhanuka <d.gunjan@iitg.ac.in>
2023-04-30 11:49:47 +0530

    added mqtt protocol

--- Cycle.ino
+++ Cycle.ino
@@ -4,6 +4,7 @@
 #include <TinyGPS++.h> // library for GPS module
 #include <SoftwareSerial.h>
 #include <Digital_Light_TSL2561.h> 
+#include <PubSubClient.h>
 
 int sda_pin_ldr = D2; 
 int scl_pin_ldr = D1;
@@ -15,15 +16,23 @@ int SDA_PIN=D2;
 int LED_PIN=D7;
 int ALARM_PIN = D0;
 
+String lats = "";
+String longs = "";
+
 TinyGPSPlus gps;  // The TinyGPS++ object
 SoftwareSerial ss(4, 5); // The serial connection to the GPS device
-const char* ssid = "nord1"; //ssid of your wifi
-const char* password = "samit123"; //password of your wifi
 float latitude , longitude;
 int year , month , date, hour , minute , second;
 String date_str , time_str , lat_str , lng_str;
 int pm;
 WiFiServer server(80);
+
+const char* ssid = "Realme7";
+const char* password = "poke1235";
+const char* mqtt_server = "91.121.93.94";
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setupGPS();
 void sendGPSData();
@@ -37,38 +46,53 @@ void runLDR();
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
-  setupMPU();
-  checkMPUSettings();
+  // setupMPU();
+  // checkMPUSettings();
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(ALARM_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   digitalWrite(ALARM_PIN, LOW);
   // setupLDR();
-
+  setupMQTT();
   // setupGPS();
+
+  
 }
 
 void loop()
 {
   // runLDR();
-  Activites act = mpu.readActivites();
-  if (act.isActivity)
-  {
-    digitalWrite(LED_PIN, HIGH);
-    digitalWrite(ALARM_PIN, HIGH);
-    Serial.println("*************ACTIVITY DETECTED*********");
-    delay(BUZZ_TIME);
-    // Send gps data
-    // sendGPSData();
-  } else
-  {
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(ALARM_PIN, LOW);
+  // Activites act = mpu.readActivites();
+  // if (act.isActivity)
+  // {
+  //   digitalWrite(LED_PIN, HIGH);
+  //   digitalWrite(ALARM_PIN, HIGH);
+  //   Serial.println("*************ACTIVITY DETECTED*********");
+  //   delay(BUZZ_TIME);
+  //   // Send gps data
+  //   // sendGPSData();
+  // } else
+  // {
+  //   digitalWrite(LED_PIN, LOW);
+  //   digitalWrite(ALARM_PIN, LOW);
+  // }
+  if(!client.connected()){
+    reconnect();
   }
 
+
+  // sendGPSData();
+  lats = "abc";
+  longs = "abc";
+  Serial.println("Sending MQTT data..");
+  char buf[10];
+  lats.toCharArray(buf, 6);
+  client.publish("gunjan/gps", buf);
+  longs.toCharArray(buf, 6);
+  client.publish("gunjan/gps", buf);
   delay(50);
 }
 
--- GPS_Module.ino
+++ GPS_Module.ino
@@ -1,7 +1,9 @@
 
 void setupGPS(){
-  ss.begin(115200);
-  Serial.println("Setting up GPS");
+  Serial.begin(9600);
+  ss.begin(4800);
+  
+  Serial.println("Set up GPS done!");
   // Serial.println();
   // Serial.print("Connecting to ");
   // Serial.println(ssid);
@@ -27,68 +29,74 @@ void sendGPSData()
   
     if (gps.encode(ss.read())) //read gps data
     {
-      if (gps.location.isValid()) //check whether gps location is valid
-      {
-        latitude = gps.location.lat();
-        lat_str = String(latitude , 6); // latitude location is stored in a string
-        longitude = gps.location.lng();
-        lng_str = String(longitude , 6); //longitude location is stored in a string
-      }
-      if (gps.date.isValid()) //check whether gps date is valid
-      {
-        date_str = "";
-        date = gps.date.day();
-        month = gps.date.month();
-        year = gps.date.year();
-        if (date < 10)
-          date_str = '0';
-        date_str += String(date);// values of date,month and year are stored in a string
-        date_str += " / ";
+      latitude = gps.location.lat();
+      lat_str = String(latitude , 6); // latitude location is stored in a string
+      longitude = gps.location.lng();
+      lng_str = String(longitude , 6); //longitude location is stored in a string
+      lats = lat_str;
+      longs = lng_str;
+      // if (gps.location.isValid()) //check whether gps location is valid
+      // {
+      //   latitude = gps.location.lat();
+      //   lat_str = String(latitude , 6); // latitude location is stored in a string
+      //   longitude = gps.location.lng();
+      //   lng_str = String(longitude , 6); //longitude location is stored in a string
+      // }
+      // if (gps.date.isValid()) //check whether gps date is valid
+      // {
+      //   date_str = "";
+      //   date = gps.date.day();
+      //   month = gps.date.month();
+      //   year = gps.date.year();
+      //   if (date < 10)
+      //     date_str = '0';
+      //   date_str += String(date);// values of date,month and year are stored in a string
+      //   date_str += " / ";
 
-        if (month < 10)
-          date_str += '0';
-        date_str += String(month); // values of date,month and year are stored in a string
-        date_str += " / ";
-        if (year < 10)
-          date_str += '0';
-        date_str += String(year); // values of date,month and year are stored in a string
-      }
-      if (gps.time.isValid())  //check whether gps time is valid
-      {
-        time_str = "";
-        hour = gps.time.hour();
-        minute = gps.time.minute();
-        second = gps.time.second();
-        minute = (minute + 30); // converting to IST
-        if (minute > 59)
-        {
-          minute = minute - 60;
-          hour = hour + 1;
-        }
-        hour = (hour + 5) ;
-        if (hour > 23)
-          hour = hour - 24;   // converting to IST
-        if (hour >= 12)  // checking whether AM or PM
-          pm = 1;
-        else
-          pm = 0;
-        hour = hour % 12;
-        if (hour < 10)
-          time_str = '0';
-        time_str += String(hour); //values of hour,minute and time are stored in a string
-        time_str += " : ";
-        if (minute < 10)
-          time_str += '0';
-        time_str += String(minute); //values of hour,minute and time are stored in a string
-        time_str += " : ";
-        if (second < 10)
-          time_str += '0';
-        time_str += String(second); //values of hour,minute and time are stored in a string
-        if (pm == 1)
-          time_str += " PM ";
-        else
-          time_str += " AM ";
-      }
+      //   if (month < 10)
+      //     date_str += '0';
+      //   date_str += String(month); // values of date,month and year are stored in a string
+      //   date_str += " / ";
+      //   if (year < 10)
+      //     date_str += '0';
+      //   date_str += String(year); // values of date,month and year are stored in a string
+      // }
+      // if (gps.time.isValid())  //check whether gps time is valid
+      // {
+      //   time_str = "";
+      //   hour = gps.time.hour();
+      //   minute = gps.time.minute();
+      //   second = gps.time.second();
+      //   minute = (minute + 30); // converting to IST
+      //   if (minute > 59)
+      //   {
+      //     minute = minute - 60;
+      //     hour = hour + 1;
+      //   }
+      //   hour = (hour + 5) ;
+      //   if (hour > 23)
+      //     hour = hour - 24;   // converting to IST
+      //   if (hour >= 12)  // checking whether AM or PM
+      //     pm = 1;
+      //   else
+      //     pm = 0;
+      //   hour = hour % 12;
+      //   if (hour < 10)
+      //     time_str = '0';
+      //   time_str += String(hour); //values of hour,minute and time are stored in a string
+      //   time_str += " : ";
+      //   if (minute < 10)
+      //     time_str += '0';
+      //   time_str += String(minute); //values of hour,minute and time are stored in a string
+      //   time_str += " : ";
+      //   if (second < 10)
+      //     time_str += '0';
+      //   time_str += String(second); //values of hour,minute and time are stored in a string
+      //   if (pm == 1)
+      //     time_str += " PM ";
+      //   else
+      //     time_str += " AM ";
+      // }
 
       Serial.println("lat: ");
       Serial.println(lat_str);
